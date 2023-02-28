810f60a732f8f63fb7832d1dbf70da2b9d41b3f0
katherine-j-c-s <kathijcs@gmail.com>
2023-02-28 13:43:53 -0300

    finished introtocs homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,36 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   
+   let numerosBinarios = num.split("") ;
+   let bi = 2;
+   let posicion = numerosBinarios.length - 1;
+   let numeros = [];
 
-function DecimalABinario(num) {}
+   for (let n = 0; n < numerosBinarios.length; n++) {
+      if (numerosBinarios[n] == 1) {
+         let elevado = bi ** posicion; 
+         numeros.push(elevado);
+      }
+      posicion --;
+   }
+
+   let suma = 0 ;
+
+   for (let n = 0; n < numeros.length; n++) {
+      suma = suma + numeros[n];
+   }
+
+   let numeroDecimal = suma;
+
+   return numeroDecimal;
+}
+function DecimalABinario(num) {
+   let numero = num;
+   let numBinario = numero.toString(2);
+
+   return numBinario;
+}
 
 module.exports = {
    BinarioADecimal,
