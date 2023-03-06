85787ac95543d9e23d68275a2351f0a7fdd6e026
katherine-j-c-s <kathijcs@gmail.com>
2023-03-06 20:53:47 -0300

    safe half exercises of homework 6

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -9,8 +9,65 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {}
+class BinarySearchTree {
+   constructor(){
+      this.root = null;
+      this._lenght = 0;
+   }
+   bts(value){
+      let current = this.root.value;
+      let valor = value.value;
+      let haySiguiente = true;
 
+      while (haySiguiente) {
+         if (valor > current) {
+            if (current.right === null) {
+               current.right = value;
+               haySiguiente = false;
+            }else{
+               current = current.right.value;
+            } 
+         }else if (value.value <= current.value){
+            if (current.left === null) {
+               current.left = value;
+               haySiguiente = false;
+            }else{
+               current = current.left.value;
+            }
+         }
+      }
+   }
+}
+BinarySearchTree.prototype.size = function() {
+   return this._lenght;
+}
+BinarySearchTree.prototype.insert = function(value) {
+   if (this.root === null) {
+      this.root = value;
+   }
+   this.bts(value)
+   this._lenght++;
+   return this.root
+}
+class Node {
+   constructor(value){
+      this.value = value;
+      this.left = null;
+      this.right = null;
+   }
+}
+
+let listaBTS = new BinarySearchTree();
+
+let node1 = new Node(8);
+let node2 = new Node(3);
+let node3 = new Node(10);
+
+listaBTS.insert(node1)
+listaBTS.insert(node2)
+listaBTS.insert(node3)
+
+console.log(listaBTS);
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
