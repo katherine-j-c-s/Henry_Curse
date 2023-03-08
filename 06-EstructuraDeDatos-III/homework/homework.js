baa662ec1b573496ec02efa795b589500a5bbea7
katherine-j-c-s <kathijcs@gmail.com>
2023-03-08 15:31:51 -0300

    finished 06 and 07

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /*
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
   - size: retorna la cantidad total de nodos del árbol
@@ -9,63 +8,100 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
+//VAMOS A APLICAR RECURCIONES PARA PODER RECORRERLO LAS VECES NECESARIAS
 class BinarySearchTree {
-   constructor(){
-      this.root = null;
-      this._lenght = 0;
-   }
-   bts(value){
-      let current = this.root.value;
-      let valor = value.value;
-      let haySiguiente = true;
-
-      while (haySiguiente) {
-         if (valor > current) {
-            if (current.right === null) {
-               current.right = value;
-               haySiguiente = false;
-            }else{
-               current = current.right.value;
-            } 
-         }else if (value.value <= current.value){
-            if (current.left === null) {
-               current.left = value;
-               haySiguiente = false;
-            }else{
-               current = current.left.value;
-            }
-         }
-      }
-   }
-}
-BinarySearchTree.prototype.size = function() {
-   return this._lenght;
-}
-BinarySearchTree.prototype.insert = function(value) {
-   if (this.root === null) {
-      this.root = value;
-   }
-   this.bts(value)
-   this._lenght++;
-   return this.root
-}
-class Node {
    constructor(value){
       this.value = value;
       this.left = null;
       this.right = null;
    }
 }
+BinarySearchTree.prototype.size = function() {
+   if (!this.left && !this.right) {//no tiene nada en la izq y derch
+      return 1
+   }
+   if (!this.left) return 1 + this.right.size()//si no tiene nada la izq verifica si hay algo a la derecha
+   if (!this.left) return 1 + this.right.size()//la derecha es null y se verifica si en la izq hay algo
+   if (this.left && this.right) return 1 + this.left.size() + this.right.size() // si ambos tiene algo , suma sus sizes con el root(1)
+}
+BinarySearchTree.prototype.insert = function(value) {
+   if (value > this.value) {//si es mayor
+      if (!this.right) { //si esta vacio
+         this.right =new BinarySearchTree(value);//agrega nuevo arbol creado
+      } else {
+         this.right.insert(value);//va al valor del siguiente y vuelve a recorrer
+      }
+   }else {
+      if (!this.left) { //si esta vacio
+         this.left =new BinarySearchTree(value);//agrega nuevo arbol creado
+      } else {
+         this.left.insert(value);//va al valor del siguiente y vuelve a recorrer
+      }
+   }
+}
+BinarySearchTree.prototype.contains = function (value) {
+   if (value === this.value)return true //si es igual al root
+   if (value > this.value) {//si es mayor
+      if (this.right === null) {//si no hay nada
+         return false
+      }else {//si hay algo
+         return this.right.contains(value)//lo vuerves a recursar al valor con el contains
+      }
+   }if (value <= this.value) {//si es menor
+      if (this.left === null) {//si no hay nada
+         return false
+      }else {
+         return this.left.contains(value)//lo vuerves a recursar al valor con el contains
+      }
+   }
+}
+BinarySearchTree.prototype.depthFirstForEach = function (cb, orden) {
+//pasa como parametro una funcion y un tipo de orden
+//APLICAMIS LA MISMA FUNCION DE MANERA RECURSIVA (se recorre a si misma hasta que todo se cumpla)
+if (orden === "pre-order") {//----> V L R
+   cb(this.value);//agrega valor
+   this.left && this.left.breadthFirstForEach(cb,orden)//si en izquierda hay algo aplica la recursividad en ella
+   this.right && this.right.breadthFirstForEach(cb,orden)//si en derecha hay algo aplica la recursividad en ella
+}if (orden === "post-order") {//----> L R V
+   this.left && this.left.breadthFirstForEach(cb,orden)//si en izquierda hay algo aplica la recursividad en ella
+   this.right && this.right.breadthFirstForEach(cb,orden)//si en derecha hay algo aplica la recursividad en ella
+   cb(this.value);  
+}else {//in-order ---> L V R
+   this.left && this.left.breadthFirstForEach(cb,orden)//si en izquierda hay algo aplica la recursividad en ella
+   cb(this.value);
+   this.right && this.right.breadthFirstForEach(cb,orden)//si en derecha hay algo aplica la recursividad en ella
+}
+}
+BinarySearchTree.prototype.breadthFirstForEach = function (cb,queue=[]) {
+   //usamos queue para ordenar progresivamente
+   //creamos la recursividad
+   if(this.left){//si hay algo en la izq
+      queue.push(this.left)
+   }if (this.right) {//si hay algo en la derch
+      queue.push(queue)
+   }
+   cb(this.value)
+   if (queue.length > 0) {
+      queue.shift().primerObj.breadthFirstForEach(cb,queue)
+      //saca el primer objeto y le pasa la recursividad
+   }
+}
+const listaBTS = new BinarySearchTree(8);
 
-let listaBTS = new BinarySearchTree();
-
-let node1 = new Node(8);
-let node2 = new Node(3);
-let node3 = new Node(10);
+let node1 =3;
+let node2 =10;
+let node3 =1;
+let node4 =6;
+let node5 =14;
 
 listaBTS.insert(node1)
 listaBTS.insert(node2)
 listaBTS.insert(node3)
+listaBTS.insert(node4)
+listaBTS.insert(node5)
+
+listaBTS.size()
+listaBTS.contains(8)
 
 console.log(listaBTS);
 // No modifiquen nada debajo de esta linea
--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,36 +6,94 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let resultado = [1];
+  let numero = num;
+  let resto = null;
+  let dividendo= 2;
+  if (numero === 1) {
+    return [1]
+  }
+  for (let e = 0; e < numero; e++) {
+    resto = numero % dividendo;
+    if (resto === 0) {
+      resultado.push(dividendo);
+      numero = numero / dividendo
+    }if(resto === 1){
+      dividendo++
+    }
+  }
+  let addLast = numero
+  resultado.push(addLast)
+  return resultado;
 }
+console.log(factorear(1));
+// BUBBLE SORT
+//toma dos numeros y los compara
+//ve si el izqu es el menor y el derch el mayor
+//si el derecho es menor, coloca el izq en el derch y el derecho en el izquierdo
+//luego ompara el derecho con el siguiente
+//y vuelve a hacer los mismos pasos hasta que ya no haya un siguiente
+//(esto lo repite hasta que queden ordenados de menor a mayor)
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
-}
-
-
+  let cambio = true
+  while (cambio) {
+    cambio = false
+    for (let e = 0; e < array.length - 1; e++) {
+      if (array[e] > array[e+1]) {
+        let aux = array[e]
+        array[e] = array[e+1]
+        array[e+1] = aux
+        cambio = true
+      }
+    }
+  }
+  
+  return array          
+}                                                                                                                
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j >= 1; j--) {
+      //comienza desde el indice y deciende hasta ver si hay aulguno menor para cambiarlo 
+      if (array[j] < array[j-1]) {//si j (indice) es menor al anterior, poner el antenior en al indice y j en el anteior
+        //para ordenarlos de menor a mayor
+        let aux = array[j]
+        array[j] = array[j-1]
+        array[j-1] = aux
+      }
+    }
+  }
+  return array
 }
+let arr = [10, -2, -7, 4]
 
-
+console.log(insertionSort(arr));
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  //i avanza una posicion y j recorre todo el array
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i+1; j < array.length; j++) {//comienza desde el que esta adelante del indice
+      if (array[i] > array[j]) { //indice es mayor al j? si es mayor ,cambia el indice por el j  y sigue
+        let aux = array[i]
+        array[i] = array[j]
+        array[j] = aux
+      }
+      
+    }
+  }
+  return array
 }
-
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
